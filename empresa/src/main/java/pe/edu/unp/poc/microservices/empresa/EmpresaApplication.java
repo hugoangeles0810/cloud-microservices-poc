bbb360c2c98898c12303ec5f8585aa8d5ffd1334
ocjp5 <josemaria1036@gmail.com>
2020-11-26 23:10:39 -0500

    Urbaner: usuario y empresa

---
+++ empresa/src/main/java/pe/edu/unp/poc/microservices/empresa/EmpresaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unp.poc.microservices.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaApplication.class, args);
+	}
+
+}
