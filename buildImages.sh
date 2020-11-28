d58d2120be02ba7f42f28719dcab3f6bf5049a09
Hugo Ángeles <hugo.angeles.chavez@ibm.com>
2020-11-28 11:51:50 -0500

    Avance infraestructura

---
+++ buildImages.sh
@@ -0,0 +1,31 @@
+# Couriers
+docker build -t hugoangeles0810/cloud-microservices-poc-couriers couriers/
+docker push hugoangeles0810/cloud-microservices-poc-couriers
+
+# Cuponing
+docker build -t hugoangeles0810/cloud-microservices-poc-cuponing cuponing/
+docker push hugoangeles0810/cloud-microservices-poc-cuponing
+
+# Empresa
+docker build -t hugoangeles0810/cloud-microservices-poc-empresa empresa/
+docker push hugoangeles0810/cloud-microservices-poc-empresa
+
+# Pagos
+docker build -t hugoangeles0810/cloud-microservices-poc-pagos pagos/
+docker push hugoangeles0810/cloud-microservices-poc-pagos
+
+# Pedidos
+docker build -t hugoangeles0810/cloud-microservices-poc-pedidos pedidos/
+docker push hugoangeles0810/cloud-microservices-poc-pedidos
+
+# Seguridad
+docker build -t hugoangeles0810/cloud-microservices-poc-seguridad seguridad/
+docker push hugoangeles0810/cloud-microservices-poc-seguridad
+
+# Tracking
+docker build -t hugoangeles0810/cloud-microservices-poc-tracking tracking/
+docker push hugoangeles0810/cloud-microservices-poc-tracking
+
+# Usuario
+docker build -t hugoangeles0810/cloud-microservices-poc-usuario usuario/
+docker push hugoangeles0810/cloud-microservices-poc-usuario
